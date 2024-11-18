ebe353dcc2bce23a88709dc3a4163503f90933c2
Sage <heisgenburgthecook@gmail.com>
2024-11-18 22:01:14 +0000

    added a demo command

--- coffe.py
+++ coffe.py
@@ -9,8 +9,9 @@ class coffe(GroupCog, group_name="coffe", group_description="Commands in relatio
     def __init__(self, bot):
         self.bot = bot
         
-
-
+    @command(name="tierlist", description="This is a tier list of different coffe flavours")
+    async def tierlist(self, inter: discord.Interaction):
+        await inter.response.send_message("This is a test feature RN and more to be implemented later")
 
 async def setup(bot):
     await bot.add_cog(coffe(bot))
